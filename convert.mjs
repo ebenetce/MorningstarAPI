afa2fe4908a811671c919530c846df88c70d98e4
Edu Benet Cerda <ebenetce@mathworks.com>
2025-02-05 13:52:50 +0000

    added fs

--- convert.mjs
+++ convert.mjs
@@ -1,9 +1,12 @@
+import { promises as fs } from 'fs';
 import { convert } from '@scalar/postman-to-openapi';
 
 async function convertPostmanToOpenAPI() {
 
   try {
-    const result = await convert('Morningstar Direct Web Services.postman_collection.json');
+    const postmanCollection = await fs.readFile('Morningstar Direct Web Services.postman_collection.json', 'utf-8');
+    const result = await convert(postmanCollection);
+    await fs.writeFile('result.yml', result, 'utf-8');
     console.log(result)
     console.log('Conversion successful!');
 

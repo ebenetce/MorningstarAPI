4c995eb27f9010aa486d89116e5c4d86db056da2
Edu Benet Cerda <ebenetce@mathworks.com>
2025-02-05 13:41:51 +0000

    modified convert.js

--- convert.js
+++ convert.js
@@ -1,20 +1,20 @@
-import { promises as fs } from 'fs';
 import { convert } from '@scalar/postman-to-openapi';
 
 async function convertPostmanToOpenAPI() {
+
   try {
     // Read the Postman collection file
     const postmanCollection = await fs.readFile('PostmantoCollection.json', 'utf-8');
 
     // Convert the Postman collection to OpenAPI
     const result = await convert(JSON.parse(postmanCollection));
-
-    // Output the result to a file
-    await fs.writeFile('morningstar.yml', result, 'utf-8');
-
+    console.log(result)
     console.log('Conversion successful!');
+
   } catch (error) {
+
     console.error('Error converting Postman collection:', error);
+
   }
 }
 
